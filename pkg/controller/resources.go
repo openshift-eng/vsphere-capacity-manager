31d42723e0e24c51c71494a8420bc839df3afe3f
Joseph Callen <jcallen@redhat.com>
2024-05-02 16:37:13 -0400

    init

--- pkg/controller/resources.go
+++ pkg/controller/resources.go
@@ -6,14 +6,15 @@ import (
 	"log"
 	"time"
 
-	v1 "github.com/openshift-splat-team/vsphere-capacity-manager/pkg/apis/vspherecapacitymanager.splat.io/v1"
-	"github.com/openshift-splat-team/vsphere-capacity-manager/pkg/resources"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	v1 "github.com/openshift-splat-team/vsphere-capacity-manager/pkg/apis/vspherecapacitymanager.splat.io/v1"
+	"github.com/openshift-splat-team/vsphere-capacity-manager/pkg/resources"
 )
 
 type ResourceRequestReconciler struct {
